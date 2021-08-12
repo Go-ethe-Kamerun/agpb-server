a88be53684dda4f72ebfd84977d3f87547ad3f59
Egbe Eugene <agboreugene@gmail.com>
2021-08-12 13:54:36 +0100

    Added port to app run

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from agpb import app
 
 # We want to run our app from python and not the command line always
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='localhost', port=8001)
