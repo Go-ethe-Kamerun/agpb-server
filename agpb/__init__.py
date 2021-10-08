9e3ed5948ba774dced0cf7907b4c2a500735c70f
eugene233 <agboreugene@gmail.com>
2021-10-08 17:03:41 +0100

    Corrected trans directory

--- agpb/__init__.py
+++ agpb/__init__.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['TEMPLATES_AUTO_RELOAD'] = os.environ.get('TEMPLATES_AUTO_RELOAD')
-app.config['UPLOADS'] = os.getcwd() + 'agpb/db/data/trans/'
+app.config['UPLOADS'] = os.getcwd() + '/agpb/db/data/trans/'
 app.config['SERVER_ADDRESS'] = 'http://3.17.141.122'
 app.config['PLAY_AUDIO_ROUTE'] = '/api/v1/play?'
 
