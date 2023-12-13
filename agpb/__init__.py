0969b8e5200c16923cbe69e7df966d1b7670274c
eugene233 <agboreugene@gmail.com>
2023-12-13 18:09:26 +0100

    Remove config tracking from git

--- agpb/__init__.py
+++ agpb/__init__.py
@@ -3,17 +3,22 @@ import os
 from flask import Flask, request, session
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+import yaml
 
 app = Flask(__name__)
 
 
-# Another secret key will be generated later
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['TEMPLATES_AUTO_RELOAD'] = os.environ.get('TEMPLATES_AUTO_RELOAD')
-app.config['UPLOADS_DIR'] = os.getcwd() + os.environ.get('UPLOADS_DIR')
-app.config['SERVER_ADDRESS'] = os.environ.get('SERVER_ADDRESS')
-app.config['PLAY_AUDIO_ROUTE'] = os.environ.get('PLAY_AUDIO_ROUTE')
+# Load configuration from YAML file
+__dir__ = os.path.dirname(__file__)
+app.config.update(
+    yaml.safe_load(open(os.path.join(__dir__, 'config.yaml'))))
+
+app.config['SQLALCHEMY_DATABASE_URI']
+app.config['SECRET_KEY']
+app.config['TEMPLATES_AUTO_RELOAD']
+app.config['UPLOADS'] = os.getcwd() + '/agpb/db/data/trans/'
+app.config['SERVER_ADDRESS'] = 'http://3.17.141.122'
+app.config['PLAY_AUDIO_ROUTE'] = '/api/v1/play?'
 
 db = SQLAlchemy(app)
 
